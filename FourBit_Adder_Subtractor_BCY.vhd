3011960bbb6e9c75f12ded116707f3310ae89f2c
Brian Yarbrough <C16brian.yarbrough@usafa.edu>
2014-02-11 14:16:38 -0700

    subtractor

--- FourBit_Adder_Subtractor_BCY.vhd
+++ FourBit_Adder_Subtractor_BCY.vhd
@@ -19,6 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -45,14 +46,17 @@ architecture Structural of FourBit_Adder_Subtractor_BCY is
 				Xout : out STD_LOGIC);
 	end component Full_Adder_BCY;
 	
-signal Carry : STD_LOGIC_VECTOR (4 downto 0);
+signal Carry : STD_LOGIC_VECTOR (4 downto 0) ;
+signal Bin_Inv : STD_LOGIC_VECTOR (3 downto 0);
 
 begin
+		--if button is pushed, covert Bin to two's comliment for subtraction
+		Bin_Inv <= std_logic_vector(UNSIGNED(not Bin) + 1) when 1>0 else Bin;
 
 	-- this componenet by itself is a one bit adder
 	Bit0: component Full_Adder_BCY
 		port map (Ain => Ain(0),
-					 Bin => Bin(0),
+					 Bin => Bin_Inv(0),
 					 Cin => Carry(0),
 					 Cout => Carry(1),
 					 Xout => Sum(0)
@@ -61,7 +65,7 @@ begin
 	--each componensts adds a bit
 	Bit1: component Full_Adder_BCY
 		port map (Ain => Ain(1),
-					 Bin => Bin(1),
+					 Bin => Bin_Inv(1),
 					 Cin => Carry(1),
 					 Cout => Carry(2),
 					 Xout => Sum(1)
@@ -69,7 +73,7 @@ begin
 					 
 	Bit2: component Full_Adder_BCY
 		port map (Ain => Ain(2),
-					 Bin => Bin(2),
+					 Bin => Bin_Inv(2),
 					 Cin => Carry(2),
 					 Cout => Carry(3),
 					 Xout => Sum(2)
@@ -77,12 +81,11 @@ begin
 					 
 	Bit3: component Full_Adder_BCY
 		port map (Ain => Ain(3),
-					 Bin => Bin(3),
+					 Bin => Bin_Inv(3),
 					 Cin => Carry(3),
 					 Cout => Carry(4),
 					 Xout => Sum(3)
 					 );					 
-
 
 end Structural;
 
